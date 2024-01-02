374d56805b09742158476d401438308251105ffa
wuguishifu <49931047+wuguishifu@users.noreply.github.com>
2024-01-02 15:11:26 -0800

    added tauri scaffold

--- bucket-builder/vite.config.ts
+++ bucket-builder/vite.config.ts
@@ -1,25 +1,23 @@
-import { exec } from 'child_process';
-
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { NormalizedInputOptions } from 'rollup';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => ({
-  plugins: [
-    react(),
-    {
-      name: "electron-main",
-      async buildStart(options: NormalizedInputOptions) {
-        if (command === 'build') return;
-        exec('npm run dev:electron');
-      }
-    }
-  ],
+export default defineConfig({
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src")
     }
+  },
+  clearScreen: false,
+  server: {
+    strictPort: true
+  },
+  envPrefix: ['VITE_', 'TAURI_PLATFORM', 'TAURI_ARCH', 'TAURI_FAMILY', 'TAURI_PLATFORM_VERSION', 'TAURI_PLATFORM_TYPE', 'TAURI_DEBUG'],
+  build: {
+    target: process.env.TAURI_PLATFORM === 'windows' ? 'chrome105' : 'safari13',
+    minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+    sourcemap: !!process.env.TAURI_DEBUG
   }
-}));
+});
